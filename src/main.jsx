8ff9dbd505f95dfc84165e1d5af1f6bbebc26b6e
Fabricio Porras Morera <faporras05@hotmail.com>
2024-08-18 07:36:03 -0600

    Development changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Contact } from "./page/Contact/Contact.jsx";
-import "./index.css";
-import "../firebase.js"
 import { ContactAdmin } from "./page/admin/contacto-admin/ContactAdmin.jsx";
+import { CourseCard } from "./component/CourseCard.jsx";
+import "./index.css";
+import "../firebase.js";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +20,10 @@ const router = createBrowserRouter([
   {
     path: "contactoAdmin",
     element: <ContactAdmin />,
+  },
+  {
+    path: "card",
+    element: <CourseCard />,
   },
 ]);
 
