a019859f215b92af00e1438abe5bc84f26a9ab75
Fabricio Porras Morera <faporras05@hotmail.com>
2024-08-28 10:05:12 -0600

    Pr conflict solved

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./hooks/authAdmin/useAuth";
+import ProtectedRoute from "./hooks/authAdmin/ProtectedRoute";
+import ClientHome from "./page/client/ClientHome";
+import { Contact } from "./page/Contact/Contact";
+import Login from "./component/adminLogin/Login";
+import AdminManagement from "./page/admin/managementUser/adminManagement";
+import NotFound from "./page/notFound/NotFound";
+import AdminLayout from "./utils/layout/AdminLayout";
+import {CourseAdmin} from "./page/admin/courseAdmin/CourseAdmin";
+import ContactAdmin from "./page/admin/contactAdmin/ContactAdmin";
+import {Calendario} from "./page/Calendario/Calendario";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Rutas públicas para la parte de clientes */}
+          <Route path="/" element={<ClientHome />} />
+          <Route path="/contacto" element={<Contact />} />
+
+          {/* Ruta de autenticación */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Rutas protegidas para la parte de administración */}
+          <Route element={<ProtectedRoute />}>
+            <Route element={<AdminLayout />}>
+              <Route path="/admin/management" element={<AdminManagement />} />
+              <Route path="/admin/contact" element={<ContactAdmin />} />
+              <Route path="/admin/courses" element={<CourseAdmin />} />
+              <Route path="/admin/calendar" element={<Calendario />} />
+            </Route>
+          </Route>
+
+          {/* Ruta para manejar páginas no encontradas */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
