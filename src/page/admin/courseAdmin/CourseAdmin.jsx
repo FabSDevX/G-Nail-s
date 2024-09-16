9bc33d2d3eae0d442d9074d30740945e1b92c83b
Fabricio Porras Morera <faporras05@hotmail.com>
2024-09-16 17:53:01 -0600

    Update CourseAdmin.jsx

--- src/page/admin/courseAdmin/CourseAdmin.jsx
+++ src/page/admin/courseAdmin/CourseAdmin.jsx
@@ -105,7 +105,7 @@ export function CourseAdmin() {
           maxWidth: "1444px",
           height: {
             xs: "76vh",
-            md: "85vh"
+            md: "auto"
           },
           padding: "0",
           border: "none",
